202dd40cc06cc3903b30533f701e1d6dbe09fb06
HEVOD <sensmo@yandex.ru>
2017-03-27 22:10:19 +0500

    Update apply-patches.sh

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -6,7 +6,7 @@ git apply -v ../../device/dexp/ixionM255/patches/0004-libnetutils-add-MTK-bits-i
 #git apply -v ../../device/dexp/ixionM255/patches/0012-PATCH-xen0n-some-MTK-services-e.g.-ril-daemon-mtk-re.patch
 cd ../..
 cd bionic
-git apply -v --check ../device/dexp/ixionM255/patches/0002-Apply-LIBC-version-to-__pthread_gettid.patch
+#git apply -v --check ../device/dexp/ixionM255/patches/0002-Apply-LIBC-version-to-__pthread_gettid.patch
 cd ..
 cd system/sepolicy
 git apply -v ../../device/dexp/ixionM255/patches/0003-Revert-back-to-policy-version-29.patch
@@ -15,11 +15,11 @@ cd packages/apps/Settings
 git apply -v ../../../device/dexp/ixionM255/patches/0005-add-author-info-in-device-info.patch
 cd ../../..
 cd frameworks/av
-git apply -v ../../device/dexp/ixionM255/patches/0006-fix-access-wvm-to-ReadOptions.patch
-#git apply -v ../../device/dexp/ixionM255/patches/0007-Disable-usage-of-get_capture_position.patch
+#git apply -v ../../device/dexp/ixionM255/patches/0006-fix-access-wvm-to-ReadOptions.patch
+git apply -v ../../device/dexp/ixionM255/patches/0007-Disable-usage-of-get_capture_position.patch
 #git apply -v ../../device/dexp/ixionM255/patches/0008-Partial-Revert-Camera1-API-Support-SW-encoders-for-n.patch
 #git apply -v ../../device/dexp/ixionM255/patches/0009-add-mtk-color-format-support.patch
-git apply -v ../../device/dexp/ixionM255/patches/0013-fix-_ZN7android16MediaBufferGroup14acquire_bufferEPP.patch
+#git apply -v ../../device/dexp/ixionM255/patches/0013-fix-_ZN7android16MediaBufferGroup14acquire_bufferEPP.patch
 cd ../..
 cd system/netd
 git apply -v ../../device/dexp/ixionM255/patches/0010-wifi-tethering-fix.patch
