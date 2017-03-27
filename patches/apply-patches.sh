2c309c3063e4713d1cea5c0814c3f5a4eb0fcb94
HEVOD <sensmo@yandex.ru>
2017-03-27 22:05:40 +0500

    Update apply-patches.sh

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -3,7 +3,7 @@ cd ../../../..
 cd system/core
 git apply -v ../../device/dexp/ixionM255/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
 git apply -v ../../device/dexp/ixionM255/patches/0004-libnetutils-add-MTK-bits-ifc_ccmni_md_cfg.patch
-git apply -v ../../device/dexp/ixionM255/patches/0012-PATCH-xen0n-some-MTK-services-e.g.-ril-daemon-mtk-re.patch
+#git apply -v ../../device/dexp/ixionM255/patches/0012-PATCH-xen0n-some-MTK-services-e.g.-ril-daemon-mtk-re.patch
 cd ../..
 cd bionic
 git apply -v --check ../device/dexp/ixionM255/patches/0002-Apply-LIBC-version-to-__pthread_gettid.patch
@@ -18,7 +18,7 @@ cd frameworks/av
 git apply -v ../../device/dexp/ixionM255/patches/0006-fix-access-wvm-to-ReadOptions.patch
 #git apply -v ../../device/dexp/ixionM255/patches/0007-Disable-usage-of-get_capture_position.patch
 #git apply -v ../../device/dexp/ixionM255/patches/0008-Partial-Revert-Camera1-API-Support-SW-encoders-for-n.patch
-git apply -v ../../device/dexp/ixionM255/patches/0009-add-mtk-color-format-support.patch
+#git apply -v ../../device/dexp/ixionM255/patches/0009-add-mtk-color-format-support.patch
 git apply -v ../../device/dexp/ixionM255/patches/0013-fix-_ZN7android16MediaBufferGroup14acquire_bufferEPP.patch
 cd ../..
 cd system/netd
